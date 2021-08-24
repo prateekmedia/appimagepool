fe6c94a52785f7e787af4cd96f45dea4f2f57594
Prateek SU <pankajsunal123@gmail.com>
2021-08-25 00:38:12 +0530

    CI cleanup's

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 sudo chmod +x appimagetool-x86_64.AppImage
-cp -r target/release/build/* AppDir
+mkdir AppDir/data AppDir/lib
+cp -r target/release/data/* AppDir/data/
+cp -r target/release/lib/* AppDir/lib/
+cp target/release/appimagepool AppDir/appimagepool
+for f in $(find -type l);do cp --remove-destination $(readlink $f) $f;done;
+
 mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps/
 cp assets/appimagepool.png AppDir/usr/share/icons/hicolor/256x256/apps/
 cp assets/appimagepool.png AppDir/appimagepool.png
+
 mkdir -p AppDir/usr/share/applications
 cp assets/appimagepool.desktop AppDir/usr/share/applications/appimagepool.desktop
 cp assets/appimagepool.desktop AppDir/appimagepool.desktop
+
 sudo chmod +x appimagetool-x86_64.AppImage
 ARCH=x86_64 ./appimagetool-x86_64.AppImage AppDir/ appimagepool-x86_64.AppImage
