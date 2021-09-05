e075f19cf66b4e0eacb1e07bfb7b241327b65991
Prateek SU <pankajsunal123@gmail.com>
2021-09-05 12:10:18 +0530

    Fix permissions and add appdata xml

--- create_appimage.sh
+++ create_appimage.sh
@@ -8,7 +8,7 @@ echo '#!/bin/sh
 
 cd "$(dirname "$0")"
 exec ./appimagepool' > AppDir/AppRun
-sudo chmod +x AppDir/AppRun
+chmod +x AppDir/AppRun
 
 # Copy all build files to AppDir
 cp -aR target/release/data/ AppDir/data/
@@ -19,8 +19,8 @@ for f in $(find -type l);do cp --remove-destination $(readlink $f) $f;done;
 ## Add Application metadata
 # Copy app icon
 
-install -Dm777  assets/appimagepool.svg AppDir/appimagepool.svg
-sudo install -Dm777 AppDir/appimagepool.svg AppDir/usr/share/icons/hicolor/128x128/apps/appimagepool.svg
+install -Dm644  assets/appimagepool.svg AppDir/appimagepool.svg
+install -Dm644 AppDir/appimagepool.svg AppDir/usr/share/icons/hicolor/128x128/apps/appimagepool.svg
 
 # Either copy .desktop file content from file or with echo command
 # cp assets/appimagepool.desktop AppDir/appimagepool.desktop
@@ -35,9 +35,13 @@ StartupWMClass=appimagepool
 Categories=Utility;
 Keywords=AppImage;Store;AppImageHub;Flutter;Gtk;' > AppDir/appimagepool.desktop
 
-sudo install -Dm777 AppDir/appimagepool.desktop AppDir/usr/share/applications/appimagepool.desktop
+install -Dm644 AppDir/appimagepool.desktop AppDir/usr/share/applications/appimagepool.desktop
+
+# Add appdata to appimage
+install -Dm644 assets/appimagepool.appdata.xml AppDir/appimagepool.appdata.xml
+install -Dm644 AppDir/appimagepool.appdata.xml AppDir/usr/share/metainfo/appimagepool.appdata.xml
 
 ## Start build
 test ! -e appimagetool-x86_64.AppImage && curl -L https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -o appimagetool-x86_64.AppImage
-sudo chmod +x appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
 ARCH=x86_64 ./appimagetool-x86_64.AppImage AppDir/ appimagepool-x86_64.AppImage
