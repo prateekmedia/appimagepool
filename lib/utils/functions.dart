01acd94664b40161bcc02a8251aedbed32364c5d
Prateek SU <pankajsunal123@gmail.com>
2021-09-27 22:17:07 +0530

    Let's try some strategies to execute flatpak

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -29,13 +29,47 @@ runProgram({required String location, required String program}) async {
 
   if (result == 0) {
     // Execute with flatpak if app is contanerized
-    return (await Process.run(
+    var _result = (await Process.run(
       'flatpak-spawn',
-      ['--host', './$program', 'flatpak-spawn'],
+      [
+        '--host',
+        './$program',
+      ],
       workingDirectory: location,
       runInShell: true,
     ))
         .exitCode;
+    if (_result == 0) {
+      print("First strategy worked");
+      return result;
+    } else {
+      var __result = (await Process.run(
+        'flatpak-spawn',
+        [
+          '--host',
+          location + (location.endsWith('/') ? '' : '/') + './$program',
+        ],
+        runInShell: true,
+      ))
+          .exitCode;
+      if (__result == 0) {
+        print("Second strategy worked");
+        return __result;
+      } else {
+        print('Final Strategy');
+        return (await Process.run(
+          'flatpak-spawn',
+          [
+            '--host',
+            'sh',
+            '-c',
+            location + (location.endsWith('/') ? '' : '/') + './$program',
+          ],
+          runInShell: true,
+        ))
+            .exitCode;
+      }
+    }
   } else {
     // Else execute normally
     return (await Process.run(
