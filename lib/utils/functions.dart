90f3aa5a4daf31bd43a66ac2f7c68939f296f588
Prateek SU <pankajsunal123@gmail.com>
2021-09-27 23:06:27 +0530

    First strategy worked

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -29,7 +29,7 @@ runProgram({required String location, required String program}) async {
 
   if (result == 0) {
     // Execute with flatpak if app is contanerized
-    var _result = (await Process.run(
+    return (await Process.run(
       'flatpak-spawn',
       [
         '--host',
@@ -39,37 +39,6 @@ runProgram({required String location, required String program}) async {
       runInShell: true,
     ))
         .exitCode;
-    if (_result == 0) {
-      print("First strategy worked");
-      return result;
-    } else {
-      var __result = (await Process.run(
-        'flatpak-spawn',
-        [
-          '--host',
-          location + (location.endsWith('/') ? '' : '/') + './$program',
-        ],
-        runInShell: true,
-      ))
-          .exitCode;
-      if (__result == 0) {
-        print("Second strategy worked");
-        return __result;
-      } else {
-        print('Final Strategy');
-        return (await Process.run(
-          'flatpak-spawn',
-          [
-            '--host',
-            'sh',
-            '-c',
-            location + (location.endsWith('/') ? '' : '/') + './$program',
-          ],
-          runInShell: true,
-        ))
-            .exitCode;
-      }
-    }
   } else {
     // Else execute normally
     return (await Process.run(
